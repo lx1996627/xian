ecb76066451247886eabfe8d4c88f83f6448652d
blueberryliao <7773237+blueberryliao@user.noreply.gitee.com>
2020-07-16 13:33:28 +0800

    bb0716

--- nodeServer/routes/privateUsersRouter.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var userCtrl = require("../controllers/userCtrl");
-
-router.post('/getUserId', userCtrl.getUserId);
-
-module.exports = router;
---
+++ nodeServer/routes/router.js
@@ -0,0 +1,37 @@
+const express = require('express'); //使用express提供的路由方法
+const router = express.Router(); //调用express对象的路由方法来获取路由对象 Router大写
+
+const userController = require('../controller/userController');
+const menuController = require('../controller/menuController');
+const navController = require('../controller/navController');
+
+// 常见的4种请求 get post put delete 请求
+//路由拦截请求，判断请求的类型 再进行任务分发
+
+//=================================login模块=========================
+
+//登陆验证
+router.post("/login", userController.checkUserPost);
+router.post('/checkAvailability', userController.userAvailable);
+router.post('/regist', userController.regist)
+
+//=================================左侧菜单栏模块=========================
+//获取菜单栏数据
+router.get('/freshmenus', menuController.getFreshMenus);
+router.get('/drymenus', menuController.getDryMenus);
+router.get('/gifthmenus', menuController.getGiftMenus);
+
+//=================================页面渲染模块=========================
+//获取菜单栏数据
+// router.get('/freshUsage', menuController.getFreshFlowerUsage);
+// router.get('/freshMaterial', menuController.getFreshFlowerMaterial);
+// router.get('/freshType', menuController.getFreshFlowerType);
+// router.get('/freshPrice', menuController.getFreshFlowerPrice);
+
+// ==============================首页导航栏模块==========================
+router.get('/sidebar', navController.getSideBar)
+
+//注销登陆
+// router.get('/logout.do', userController.logout)
+
+module.exports = router;
